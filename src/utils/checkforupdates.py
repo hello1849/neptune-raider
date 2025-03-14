9b995b71e67899a656b9e44fdb545e66ba5fd83b
Aura Tools <qwilyybusiness@gmail.com>
2025-03-14 17:31:36 +0100

    Create checkforupdates.py

---
+++ src/utils/checkforupdates.py
@@ -0,0 +1,19 @@
+#######################################################################
+#           ___                       ______            __            #
+#          /   | __  ___________ _   /_  __/___  ____  / /____        #
+#         / /| |/ / / / ___/ __ `/    / / / __ \/ __ \/ / ___/        #
+#        / ___ / /_/ / /  / /_/ /    / / / /_/ / /_/ / (__  )         #
+#       /_/  |_\__,_/_/   \__,_/    /_/  \____/\____/_/____/          #
+#                       https://auratools.xyz/                        #
+#                    https://discord.gg/auratools                     #
+#                            Neptune Free                             #
+#######################################################################
+              
+#from src import *
+#from src.utils import *
+#from src.utils.checkforupdates import *
+#from src.utils.ui import *
+#from src.utils.moreutils import *
+
+# yeah maybe next update
+# just released dawg
